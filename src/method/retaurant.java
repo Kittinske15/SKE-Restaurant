1fd4bc4c193b690deffbb980c4c7a60b23a9c354
Kittin Vatabutr <32285869+Kittinske15@users.noreply.github.com>
2017-11-07 15:00:18 +0700

    Add files via upload

--- src/method/retaurant.java
+++ src/method/retaurant.java
@@ -3,40 +3,49 @@ package method;
 import java.util.Scanner;
 
 public class retaurant {
-	public static void menu(int collectp, int collectck, int collectcoke, int pay) {
-		System.out.print("+------ Menu ------+---- Qty ----+----- Price --+\n");
-		if (collectp > 0)
-			System.out.printf("|\tPizza\t   |\t %2d\t |\t%5d\t|\n", collectp, collectp * 250);
-		if (collectck > 0)
-			System.out.printf("|\tChickens   |\t %2d\t |\t%5d\t|\n", collectck, collectck * 120);
-		if (collectcoke > 0)
-			System.out.printf("|\tCoke\t   |\t %2d\t |\t%5d\t|\n", collectcoke, collectcoke * 45);
-		System.out.print("+-----------------------------------------------+\n");
-		System.out.printf("|\tTotal\t\t\t  %11d - |\n", collectp * 250 + collectck * 120 + collectcoke * 45);
-		System.out.print("|\t\t\t\t      --------  |\n");
-		System.out.printf("|\tCus pay\t\t\t     %8d   |\n", pay);
-		System.out.printf("|\tChange\t\t\t\t%5d\t|\n", pay - (collectp * 250 + collectck * 120 + collectcoke * 45));
-		System.out.print("|\t\t\t\t      ========  |\n");
-		System.out.print("+-----------------------------------------------+\n");
 
+	static int[] price = { 250, 120, 45 };
+	static String[] menu = { "Pizza", "Chickens", "Coke" };
+	static int[] collect = { 0, 0, 0 };
+
+	public static void menu() {
+		System.out.println("---------- Welcome to SKE Restaurant ----------");
+		for (int i = 0; i < price.length; i++) {
+			System.out.printf("%d.) %s\t %5d Baht.%n", i + 1, menu[i], price[i]);
+		}
+		System.out.printf("%d.) Total%n", menu.length + 1);
+		System.out.printf("%d.) Exit%n", menu.length + 2);
+	}
+
+	public static int total() {
+		int sum = 0;
+		for (int s = 0; s < collect.length; s++) {
+			sum += collect[s] * price[s];
+		}
+		return sum;
+	}
+
+	public static void menu(int pay) {
+		System.out.print("+------ Menu ----+---- Qty ------+----- Price --+\n");
+		for (int i = 0; i < menu.length; i++) {
+			if (collect[i] > 0)
+				System.out.printf("|\t%-8s |\t %2d\t |\t%5d\t|\n", menu[i], collect[i], collect[i] * price[i]);
+		}
+		System.out.print("+-----------------------------------------------+\n");
+		System.out.printf("|%7s%-24s  %11d - |\n", "","Total",total());
+		System.out.printf("|%39s" + "------- |\n","");
+		System.out.printf("|%7s%-24s     %8d   |\n","","cus pay", pay);
+		System.out.printf("|\tChange\t\t\t\t%5d\t|\n", pay - total());
+		System.out.printf("|%39s======= |\n","");
+
+		System.out.print("+-----------------------------------------------+\n");
+        
 	}
 
 	public static void main(String[] args) {
-
-		// TODO Auto-generated method stub
-
 		Scanner scan = new Scanner(System.in);
-		System.out.print("---------- Welcome to SKE Restaurant ----------\n");
-		System.out.printf("1.) Pizza\t %5d Baht.%n", 250);
-		System.out.printf("2.) Chickens\t %5d Baht.%n", 120);
-		System.out.printf("3.) Coke\t %5d Baht.%n", 45);
-		System.out.printf("4.) Total%n");
-		System.out.printf("5.) Exit%n");
+		menu();
 		int choice = 0;
-		int collectp = 0;
-		int collectck = 0;
-		int collectcoke = 0;
-		int total = 0;
 		int pay = 0;
 		while (true) {
 			System.out.print("Enter your choice: ");
@@ -46,22 +55,20 @@ public class retaurant {
 			} else if (choice == 4) {
 				System.out.print("Customer pay: ");
 				pay = scan.nextInt();
-				menu(collectp, collectck, collectcoke, pay);
+				menu(pay);
 
 			} else {
 				System.out.print("Enter Qantity: ");
 
 				int quantity = scan.nextInt();
 				if (choice == 1) {
-					collectp = collectp + quantity;
+					collect[0] = collect[0] + quantity;
 				} else if (choice == 2) {
-					collectck = collectck + quantity;
+					collect[1] = collect[1] + quantity;
 				} else if (choice == 3) {
-					collectcoke = collectcoke + quantity;
+					collect[2] = collect[2] + quantity;
 				}
-
 			}
-
 		}
 		System.out.print("================== Thank you ==================");
 	}
